0b62e256305e2635bd1991409a53c8f516c41258
Jason Draper <jason@drapergeek.com>
2016-09-29 10:21:24 -0700

    Setup quickalias

--- mix.exs
+++ mix.exs
@@ -30,14 +30,17 @@ defmodule Intestines.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+    [
+      {:cowboy, "~> 1.0"},
+      {:gettext, "~> 0.11"},
+      {:phoenix, "~> 1.2.1"},
+      {:phoenix_ecto, "~> 3.0"},
+      {:phoenix_html, "~> 2.6"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:quick_alias, "~> 0.1.0"},
+   ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
