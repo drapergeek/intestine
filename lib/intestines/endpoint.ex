39e9a1ae184a70b7c127d885bf230d1a92cc1a15
Jason Draper <jason@drapergeek.com>
2016-09-29 09:56:19 -0700

    Setup heroku

--- lib/intestines/endpoint.ex
+++ lib/intestines/endpoint.ex
@@ -1,8 +1,6 @@
 defmodule Intestines.Endpoint do
   use Phoenix.Endpoint, otp_app: :intestines
 
-  socket "/socket", Intestines.UserSocket
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phoenix.digest
